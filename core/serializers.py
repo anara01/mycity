b8df0422e742be9a4971099f34f5a7311dc41424
anara01 <anaraerkebaeva2@gmail.com>
2022-05-06 17:21:23 +0300

    API

--- core/serializers.py
+++ core/serializers.py
@@ -1,3 +1,5 @@
+from dataclasses import field
+from statistics import mode
 from rest_framework import serializers
 from .models import Proposal
 
@@ -6,3 +8,15 @@ class ProposalListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Proposal
         fields = ['id', 'title']
+
+
+class ProposalCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Proposal
+        fields = ['title', 'description']
+
+
+class ProposalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Proposal
+        fields = ['id', 'title', 'description', 'photo']
--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.generics import RetrieveAPIView
 from rest_framework.response import Response
 
-from .serializers import ProposalListSerializer
+from .serializers import ProposalListSerializer, \
+    ProposalCreateSerializer, ProposalSerializer
 from .models import Proposal
 
 
@@ -11,3 +13,25 @@ class ProposalListAPIView(APIView):
         proposals = Proposal.objects.all()
         proposals_json = ProposalListSerializer(proposals, many=True)
         return Response(data=proposals_json.data)
+
+
+class ProposalCreateAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        data = request.POST
+        serializer = ProposalCreateSerializer(data=data)
+        if serializer.is_valid():
+            proposal = serializer.save()
+            json_data = ProposalSerializer(instance=proposal)
+            return Response(json_data.data, 201)
+            return Response(
+                data={
+                    "message": "Data not valid",
+                    "errors": serializer.errors
+                },
+                status=400
+            )
+
+
+class ProposalRetrieveAPIView(RetrieveAPIView):
+    queryset = Proposal.objects.all()
+    serializer_class = ProposalSerializer
